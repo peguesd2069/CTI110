6d60f951ac35cffc158a8a1e8b609b96a2662f99
peguesd2069 <129396289+peguesd2069@users.noreply.github.com>
2023-04-07 14:19:55 -0400

    Add files via upload

---
+++ P1HW1.py
@@ -0,0 +1,48 @@
+Python 3.10.10 (tags/v3.10.10:aad5f6a, Feb  7 2023, 17:20:36) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====================== RESTART: C:/Users/dpegu/hmassign.py =====================
+This program calculates and displays travel expenses
+
+Enter the budget:1300
+
+Enter Your destination:Atlanta
+
+Enter amount that you will spend on gas:80
+
+Enter amount that you will spend on accomodation:400
+
+Enter amount that you will spend on food:100
+
+------------------------------travel expenses----------------------------------
+Location : Atlanta
+Initial budget : 1300
+
+Traceback (most recent call last):
+  File "C:/Users/dpegu/hmassign.py", line 19, in <module>
+    print("Fuel : " + str(gas_expenses))
+NameError: name 'gas_expenses' is not defined
+>>> 
+====================== RESTART: C:/Users/dpegu/hmassign.py =====================
+This program calculates and displays travel expenses
+
+Enter the budget: 1300
+
+Enter your destination: Atlanta
+
+Enter amount that you will spend on gas: 80
+
+Enter amount that you will spend on accommodation: 400
+
+Enter amount that you will spend on food: 100
+
+------------------------------Travel expenses----------------------------------
+Location: Atlanta
+Initial budget: 1300
+
+Fuel: 80
+Accommodation: 400
+Food: 100
+
+Total expenses: 580
+Remaining balance: 720
